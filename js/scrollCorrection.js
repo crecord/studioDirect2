affbf41e89797a1cd45848513579d884c7e0c142
Caroline Record <recordcaroline@gmail.com>
2015-01-03 17:49:40 -0500

    chrome shadow fixed + some small firefox issues

--- js/scrollCorrection.js
+++ js/scrollCorrection.js
@@ -1,8 +1,9 @@
 
-// This code does two things:
+// This code does three things:
 // One: passes the scroll action to the correct div 
 // aka, it doesn't let you scroll the content until the content is already at the top of the page
 // Two: It freezes the background scrolling when a lightbox is active. 
+// Three: stops it from scrolling up and showing the awkward chrome shadow. 
 
 
 
@@ -12,6 +13,14 @@ $(document).ready(function (){
 					
 		// this function is called whenever the user scrolls	
 		function scroll () {
+		
+		//this bit stop chrome from scrolling up and showing the shadow
+		var scrollPosTop  = $(document).scrollTop();
+		if (scrollPosTop <0){
+			$('html,body').animate({scrollTop :0 },0)
+		}
+		
+		
 		var isInfoWindow = document.getElementById("info").style.display; 
 		var isFaqsWindow = document.getElementById("faqs").style.display; 
 		if (isInfoWindow == "none" &&  isFaqsWindow== "none"){
